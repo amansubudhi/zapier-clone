bbcda0f54859191a9f839bfe876ce6a13db87f7c
amansubudhi <amansubudhi12@gmail.com>
2025-03-18 21:22:07 +0530

    Added hooks

---
+++ apps/hooks/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express"
+import client from "@repo/db/client";
+
+const app = express();
+
+app.use(express.json())
+
+//password logic
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    //store in db a new trigger
+    await client.$transaction(async tx => {
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
+
+    //push it to a queue(kafka/redis)
+})
+
+app.listen(3000)
