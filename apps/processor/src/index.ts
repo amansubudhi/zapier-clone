a171e40755a837738fe8fc1b952cc7a63f3f97bf
amansubudhi <amansubudhi12@gmail.com>
2025-03-19 23:41:34 +0530

    Added Outbox Processor

---
+++ apps/processor/src/index.ts
@@ -0,0 +1,40 @@
+import client from "@repo/db/client";
+import { Kafka, Partitioners } from "kafkajs";
+
+const kafka = new Kafka({
+    clientId: 'outbox-processor',
+    brokers: ['localhost:9092']
+})
+
+//Add this global variable to packages
+const TOPIC_NAME = 'zap-events'
+
+async function main() {
+    const producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
+    await producer.connect();
+
+    while (1) {
+        const pendingRows = await client.zapRunOutbox.findMany({
+            where: {},
+            take: 10
+        })
+
+        producer.send({
+            topic: TOPIC_NAME,
+            messages: pendingRows.map(r => ({
+                value: r.zapRunId
+            }))
+        })
+
+        await client.zapRunOutbox.deleteMany({
+            where: {
+                id: {
+                    in: pendingRows.map(x => x.id)
+                }
+            }
+        })
+    }
+}
+
+main();
+
